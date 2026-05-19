f5ca5fbd1b499bcf65fd0737e2b0b8e69739695f
GioJung97 <gio6141@gmail.com>
2026-05-19 18:44:46 +0000

    Deploying to gh-pages from @ GioJung97/GioJung97.github.io@d64ee86431e0120dfcbd1ad93dffba732b284175 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,7 +37,14 @@ ninja.data = [{
               handler: () => {
                 window.location.href = "/hobbies/";
               },
-            },{id: "post-google-gemini-updates-flash-1-5-gemma-2-and-project-astra",
+            },{id: "nav-snapshots",
+          title: "Snapshots",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/snapshots/";
+          },
+        },{id: "post-google-gemini-updates-flash-1-5-gemma-2-and-project-astra",
         
           title: 'Google Gemini updates: Flash 1.5, Gemma 2 and Project Astra <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
         
@@ -93,6 +100,11 @@ ninja.data = [{
           description: "Self-supervised learning meets multi-object classification with SimCLR on COCO-annotated data",
           section: "Projects",handler: () => {
               window.location.href = "/projects/finetune_simclr/";
+            },},{id: "projects-gmis-research-competition-poster-presentation",
+          title: 'GMIS Research Competition Poster Presentation',
+          description: "GMIS Research Competition Poster Presentation",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/gmis_poster/";
             },},{id: "projects-master-39-s-thesis",
           title: 'Master&amp;#39;s Thesis',
           description: "Title: &quot;Evaluation of Vision Language Models using Item Response Theory&quot;",
